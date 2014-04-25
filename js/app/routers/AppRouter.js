cacacc62836ef5a9a4323a1f6d57432dd75b6f98
Arun Malik <akmalik@ymail.com>
2014-04-25 12:07:20 +1000

    added splash and icon

--- js/app/routers/AppRouter.js
+++ js/app/routers/AppRouter.js
@@ -12,18 +12,4 @@ app.routers.AppRouter = Backbone.Router.extend({
   login: function(){
     app.slider.slidePage(app.loginView.render().$el);
   },
-
-  home: function () {
-    // Since the home view never changes, we instantiate it and render it only once
-    if (!app.homeView) {
-        app.catagoryModel = new app.models.Catagory();
-        app.homeView = new app.views.HomeView({model: app.catagoryModel});
-        app.homeView.render();
-    } else {
-        console.log('reusing home view');
-        app.homeView.delegateEvents(); // delegate events when the view is recycled
-    }
-    // app.slider.slidePage(app.homeView.$el);
-    $('#app').html(app.homeView.$el);
-  },
 });
