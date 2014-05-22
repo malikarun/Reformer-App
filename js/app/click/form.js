c4365604906ac50833e5ee3ab6544c89d69cfc37
Arun Malik <akmalik@ymail.com>
2014-05-22 23:27:22 +1000

    added font-awesome.min.css

--- js/app/click/form.js
+++ js/app/click/form.js
@@ -2,6 +2,7 @@ $(document).on("ready", function () {
   $( "body" ).on( "click", '.submit-form', function( event ) {
     var form = $(this).closest('form');
     $.post(form.attr('action'), form.serialize()).done(function(data){
+      window.scrollTo(0,0)
       window.location = form.attr('target');
     });
   });
