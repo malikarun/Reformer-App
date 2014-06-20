3ed4dad3709f74a3d214d3e25a5168d682371fde
Arun Malik <akmalik@ymail.com>
2014-06-20 12:00:49 +1000

    minor

--- js/app/logic/afterSlide.js
+++ js/app/logic/afterSlide.js
@@ -1,7 +1,5 @@
 $(document).on('page-slided', function(){
-  console.log($('time').length);
   $('time').each(function( index ) {
-    console.log($(this).text());
     time = moment($(this).text()).fromNow();
     $(this).text(time);
   });
--- js/app/views/ReformersView.js
+++ js/app/views/ReformersView.js
@@ -1,12 +1,17 @@
 app.views.ReformersView = Backbone.View.extend({
   initialize: function(){
-    this.listenTo(this.collection, 'change', this.render);
-    this.listenTo(this.collection, 'add', this.render);
-    this.listenTo(this.collection, 'remove', this.render);
+    this.listenTo(this.collection, 'change', this.update);
+    this.listenTo(this.collection, 'add', this.update);
+    this.listenTo(this.collection, 'remove', this.update);
   },
 
   render: function () {
     this.$el.html(Handlebars.compile(this.template())(this.collection));
     return this;
   },
+
+  update: function() {
+    this.render();
+    $(document).trigger('page-slided');
+  }
 });
