d256bb0265fabf9f2a35b12fb9ee640d5d5cadfc
Arun Malik <akmalik@ymail.com>
2014-06-20 11:39:54 +1000

    minor change

--- js/app/views/IdeasView.js
+++ js/app/views/IdeasView.js
@@ -1,13 +1,17 @@
 app.views.IdeasView = Backbone.View.extend({
   initialize: function(){
-    this.listenTo(this.collection, 'change', this.render);
-    this.listenTo(this.collection, 'add', this.render);
-    this.listenTo(this.collection, 'remove', this.render);
+    this.listenTo(this.collection, 'change', this.update);
+    this.listenTo(this.collection, 'add', this.update);
+    this.listenTo(this.collection, 'remove', this.update);
   },
 
   render: function () {
     this.$el.html(Handlebars.compile(this.template())(this.collection));
     return this;
-    $(document).trigger('page-slided');
   },
+
+  update: function() {
+    this.render();
+    $(document).trigger('page-slided');
+  }
 });
