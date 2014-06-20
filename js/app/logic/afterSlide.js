e267c31e34b0a2e3cf8b69309756f5904fad8bee
Arun Malik <akmalik@ymail.com>
2014-06-20 11:57:50 +1000

    minor

--- js/app/logic/afterSlide.js
+++ js/app/logic/afterSlide.js
@@ -1,8 +1,8 @@
 $(document).on('page-slided', function(){
   console.log($('time').length);
   $('time').each(function( index ) {
-    alert(moment($(this).val()).fromNow());
-    time = moment($(this).val()).fromNow();
-    $(this).val(time);
+    console.log($(this).text());
+    time = moment($(this).text()).fromNow();
+    $(this).text(time);
   });
 });
